87e835b0ccb86f21d13cc9c003ef13c28f6bfff9
Rachel Grimwood <rachelgrimwood@developersinstitute.co.nz>
2022-06-23 16:01:33 +1200

    initialise structure'

---
+++ src/components/Attribution.js
@@ -0,0 +1,17 @@
+import "./Attribution.css";
+
+const Attribution = () => {
+  return (
+    <footer class="attribution">
+      <p>
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" rel="noreferrer">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/rachelgrimwood">Rachel Grimwood</a>.
+      </p>
+    </footer>
+  );
+};
+
+export default Attribution;
---
+++ src/components/Countdown.js
@@ -0,0 +1,9 @@
+const Countdown = () => {
+  return (
+    <main>
+      <p>We're launching soon Days Hours Minutes Seconds</p>
+    </main>
+  );
+};
+
+export default Countdown;
