b1c956b8e17f86302659690578d283cfa2ad1223
Rachel Grimwood <rachelgrimwood@developersinstitute.co.nz>
2022-06-24 14:31:01 +1200

    style text

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,7 +1,8 @@
 const Countdown = () => {
   return (
-    <div className="h-3/4 bg-[url('../src/images/bg-stars.svg')]">
-      <p>We're launching soon Days Hours Minutes Seconds</p>
+    <div className="h-3/4 bg-[url('../src/images/bg-stars.svg')] uppercase font-redhat px-4 flex flex-col justify-center">
+      <h1 className="text-white text-2xl tracking-[0.3em]">We're launching soon</h1>
+      <p className="text-grayish-blue text-xs tracking-[0.2em]">Days Hours Minutes Seconds</p>
     </div>
   );
 };
