f87cd38082658193213834d547e3af7f2c4c1aee
Rachel Grimwood <rachelgrimwood@developersinstitute.co.nz>
2022-06-24 14:10:47 +1200

    add background

--- src/components/Countdown.js
+++ src/components/Countdown.js
@@ -1,6 +1,6 @@
 const Countdown = () => {
   return (
-    <div className="h-3/4">
+    <div className="h-3/4 bg-[url('../src/images/bg-stars.svg')]">
       <p>We're launching soon Days Hours Minutes Seconds</p>
     </div>
   );
