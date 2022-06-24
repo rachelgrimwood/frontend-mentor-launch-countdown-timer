eeeb32b98cd9a6e2c6d3c4c5fd06e68b64b12f7a
Rachel Grimwood <rachelgrimwood@developersinstitute.co.nz>
2022-06-24 13:53:32 +1200

    add bottom component

---
+++ src/components/Socials.js
@@ -0,0 +1,7 @@
+const Socials = () => {
+  return (
+    <div className="h-1/4 bg-[url('../src/images/pattern-hills.svg')] bg-no-repeat bg-right-bottom bg-cover"></div>
+  );
+};
+
+export default Socials;
