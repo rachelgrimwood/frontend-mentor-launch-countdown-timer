fe9bbd0f27c4ed2ddc4d302d3365dddc1896e3b5
Rachel Grimwood <rachelgrimwood@developersinstitute.co.nz>
2022-06-27 17:08:11 +1200

    attribution color contrast

--- src/components/Attribution.js
+++ src/components/Attribution.js
@@ -1,17 +1,17 @@
 const Attribution = () => {
   return (
-    <footer className="attribution text-[11px] text-grayish-blue bg-[#2F2439] pt-10 md:pt-5">
-      <p>
+    <footer className="attribution text-[11px] text-white bg-[#2F2439] pt-10 md:pt-5">
+      <p className="bg-blue-black">
         Challenge by{" "}
         <a
-          className="text-links"
+          className="text-grayish-blue"
           href="https://www.frontendmentor.io?ref=challenge"
           target="_blank"
           rel="noreferrer">
           Frontend Mentor
         </a>
         . Coded by{" "}
-        <a className="text-links" href="https://github.com/rachelgrimwood">
+        <a className="text-grayish-blue" href="https://github.com/rachelgrimwood">
           Rachel Grimwood
         </a>
         .
