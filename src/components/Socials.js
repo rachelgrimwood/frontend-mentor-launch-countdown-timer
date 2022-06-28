f3a0b6e969b4b6e753c951e61b93279f79a5aee3
Rachel Grimwood <rachelgrimwood@developersinstitute.co.nz>
2022-06-28 14:35:20 +1200

    rework social icons

--- src/components/Socials.js
+++ src/components/Socials.js
@@ -1,37 +1,25 @@
+import { ReactComponent as FacebookLogo } from "../images/icon-facebook.svg";
+import { ReactComponent as PinterestLogo } from "../images/icon-pinterest.svg";
+import { ReactComponent as InstagramLogo } from "../images/icon-instagram.svg";
+
+const Logo = ({ SvgComponent, title }) => {
+  return (
+    <a
+      className="block h-6 w-6 sm:h-8 sm:w-8"
+      href="https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-"
+      title={title}>
+      <SvgComponent className="sm:scale-125 origin-top-left fill-grayish-blue hover:fill-soft-red" />
+    </a>
+  );
+};
+
 const Socials = () => {
   return (
     <div className="h-1/5 bg-[url('../src/images/pattern-hills.svg')] bg-no-repeat [background-position:85%_bottom] md:[background-position:right_top] bg-cover">
       <div className="h-full md:pt-20 flex justify-center items-end md:items-center gap-8 sm:gap-10 md:gap-12">
-        <a
-          className="block h-6 w-6 sm:h-8 sm:w-8"
-          href="https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-"
-          title="Facebook">
-          <img
-            className="h-6 w-6 sm:h-8 sm:w-8"
-            src={require("../images/icon-facebook.svg").default}
-            alt="Facebook"
-          />
-        </a>
-        <a
-          className="block h-6 w-6 sm:h-8 sm:w-8"
-          href="https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-"
-          title="Pinterest">
-          <img
-            className="h-6 w-6 sm:h-8 sm:w-8"
-            src={require("../images/icon-pinterest.svg").default}
-            alt="Pinterest"
-          />
-        </a>
-        <a
-          className="block h-6 w-6 sm:h-8 sm:w-8"
-          href="https://www.frontendmentor.io/challenges/launch-countdown-timer-N0XkGfyz-"
-          title="Instagram">
-          <img
-            className="h-6 w-6 sm:h-8 sm:w-8"
-            src={require("../images/icon-instagram.svg").default}
-            alt="Instagram"
-          />
-        </a>
+        <Logo SvgComponent={FacebookLogo} title="Facebook" />
+        <Logo SvgComponent={PinterestLogo} title="Pinterest" />
+        <Logo SvgComponent={InstagramLogo} title="Instagram" />
       </div>
     </div>
   );
