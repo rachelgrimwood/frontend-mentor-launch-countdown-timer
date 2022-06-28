6d201284b9a3e70a6e70e7041ec62b1c41c18fca
Rachel Grimwood <rachelgrimwood@developersinstitute.co.nz>
2022-06-28 16:03:48 +1200

    text size

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,7 +3,7 @@ import Countdown from "./Countdown";
 const Main = () => {
   return (
     <div className="h-4/5 bg-[url('../src/images/bg-stars.svg')] uppercase font-redhat px-4 flex flex-col justify-center">
-      <h1 className="text-white w-72 sm:w-full my-0 mx-auto text-xl md:text-2xl tracking-[0.3em]">
+      <h1 className="text-white w-72 sm:w-full my-0 mx-auto text-xl md:text-[1.35rem] tracking-[0.3em]">
         We're launching soon
       </h1>
       <Countdown />
